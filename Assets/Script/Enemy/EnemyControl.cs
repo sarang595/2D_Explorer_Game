edd9c9802263eba7a34d8cd084c4eae33f55457b
Saran <sarang595@gmail.com>
2025-07-31 19:59:23 +0530

    Updated EnemyAI chasing (if player goes out of range after enters into boundary enemy flip and chase till the patrol range)

--- Assets/Script/Enemy/EnemyControl.cs
+++ Assets/Script/Enemy/EnemyControl.cs
@@ -18,7 +18,7 @@ public class EnemyControl : MonoBehaviour
     float Originalx; // Original LocalScale X axis Component
     Vector3 Scale; // To store Localscale
     Vector3 EnemyDirection; // To store the Enemy direction
-    [Range(-180, 360)]
+    [Range(0, 180)]
     [SerializeField] float FovAngle; // To specify FOV Angle according to the need
     [Range(0, 10)]
     [SerializeField] float FovRadius; //  To specify Fov Radius according to the need
@@ -30,10 +30,11 @@ public class EnemyControl : MonoBehaviour
     public GameObject Projectile;
     public Transform ProjectilePos;
     public float Projectileforce;
-    private Awaitable CurrentAttack;
     public float attackCooldown = .25f;
     private float attackTimer = 0f;
     private bool isAttacking = false;
+    Vector2 toPlayer;
+    bool isChase = false;
     public enum EnemyMode { Patrol, Survilance }
     [SerializeField] public EnemyMode enemyMode = EnemyMode.Patrol; // Only this will show in inspector
     private void Start()
@@ -43,11 +44,11 @@ public class EnemyControl : MonoBehaviour
     }
 
 
-    private void Update()
+    private async void Update()
     {
         PatrolCheck();
-        EnemyMaxDirection();
-        PlayerenteredBoundary();
+        //EnemyMaxDirection();
+        await PlayerenteredBoundary();
 
         if (attackTimer > 0f)
             attackTimer -= Time.deltaTime;
@@ -64,7 +65,7 @@ public class EnemyControl : MonoBehaviour
         {
             survilance();
         }
-      
+
     }
     Vector3 getDirection()
     {
@@ -161,48 +162,98 @@ public class EnemyControl : MonoBehaviour
         }
         transform.localScale = Scale;
     }
+    //private void OnDrawGizmos()
+    //{     // Visualizing the gizmos in editor to enemy positioning and customization
+    //    Gizmos.color = Color.red;
+    //    float length = (Player.position - transform.position).magnitude;
+    //    Gizmos.DrawLine(transform.position, transform.position + (Vector3)toPlayer * length);
 
-    private void OnDrawGizmos()
+    //}
+    //private void OnDrawGizmosSelected()
+    //{
+    //    // Visualizing the gizmos in editor to enemy positioning and customization
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawLine(transform.position, transform.position + EnemyDirection * 2);
+    //    UnityEditor.Handles.color = new Color(0, 1, 0, 0.05f);
+    //    UnityEditor.Handles.DrawSolidArc(transform.position, Vector3.forward, getDirection(), relativeangle(), FovRadius);
+    //    EnemyMaxDirection();
+    //    Gizmos.DrawLine(transform.position, transform.position + EnemyMaxDirection() * 2);
+    //    Gizmos.color = Color.red;
+    //    // Gizmos.DrawLine(transform.position,transform.position+ Playerlookup * 2);
+    //}
+    private void OnDrawGizmosSelected()
     {
-        // Visualizing the gizmos in editor to enemy positioning and customization
+        // Draw forward direction
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + EnemyDirection * 2);
+        Gizmos.DrawLine(transform.position, transform.position + getDirection() * 2);
+
+        // Draw FOV cone edges
+        Vector3 edgeRight = EnemyMaxDirection(true);
+        Vector3 edgeLeft = EnemyMaxDirection(false);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + edgeRight * FovRadius);
+        Gizmos.DrawLine(transform.position, transform.position + edgeLeft * FovRadius);
+
+#if UNITY_EDITOR
+        // Draw the FOV arc (requires UnityEditor)
         UnityEditor.Handles.color = new Color(0, 1, 0, 0.05f);
-        UnityEditor.Handles.DrawSolidArc(transform.position, Vector3.forward, getDirection(), relativeangle(), FovRadius);
-        EnemyMaxDirection();
-        Gizmos.DrawLine(transform.position, transform.position + EnemyMaxDirection() * 2);
-        Gizmos.color = Color.red;
-        // Gizmos.DrawLine(transform.position,transform.position+ Playerlookup * 2);
+        Vector3 fwd = getDirection();
+        UnityEditor.Handles.DrawSolidArc(
+            transform.position,
+            Vector3.forward,
+            Quaternion.Euler(0, 0, -FovAngle / 2) * fwd,
+            FovAngle,
+            FovRadius
+        );
+#endif
     }
-    float relativeangle()
+
+    //float relativeangle()
+    //{
+    //    //Changing the FOV angle to counter the direction flip
+    //    if (!FacingRight)
+    //    {
+    //        return -FovAngle;
+    //    }
+    //    else
+    //    {
+    //        return FovAngle;
+    //    }
+    //}
+    //private Vector3 EnemyMaxDirection()
+    //{
+    //    // Calculating the Enemy Max FOV Vector using the know FOV (Serialized)
+    //    float angleInRadians = FovAngle * Mathf.Deg2Rad;
+
+    //    if (FacingRight)
+    //    {
+    //        enemymaxDir = new Vector3(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians), 0);
+    //    }
+    //    else
+    //    {
+
+    //        enemymaxDir = new Vector3(-Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians), 0);
+    //    }
+
+    //    return enemymaxDir;
+    //}
+    private Vector3 EnemyMaxDirection(bool rightEdge)
     {
-        //Changing the FOV angle to counter the direction flip
-        if (!FacingRight)
-        {
-            return -FovAngle;
-        }
-        else
-        {
-            return FovAngle;
-        }
+        // rightEdge: true for the +FOV/2 edge, false for the -FOV/2 edge
+        // Calculate half the field of view in radians
+        float halfFovRad = (FovAngle / 2f) * Mathf.Deg2Rad;
+        // Choose whether we want the right or left edge (+/- half the FOV)
+        float angleOffset = rightEdge ? halfFovRad : -halfFovRad;
+        //  Get the base forward direction (either right or left)
+        Vector2 forward = FacingRight ? Vector2.right : Vector2.left;
+        //  Find the angle (in radians) of that forward direction
+        float baseAngle = Mathf.Atan2(forward.y, forward.x);
+        //  Add the desired FOV offset (positive or negative) to get the final direction angle
+        float totalAngle = baseAngle + angleOffset;
+        //  Convert the polar (angle) back into a Vector3 direction (on the XY plane)
+        return new Vector3(Mathf.Cos(totalAngle), Mathf.Sin(totalAngle), 0f);
     }
-    private Vector3 EnemyMaxDirection()
-    {
-        // Calculating the Enemy Max FOV Vector using the know FOV (Serialized)
-        float angleInRadians = FovAngle * Mathf.Deg2Rad;
 
-        if (FacingRight)
-        {
-            enemymaxDir = new Vector3(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians), 0);
-        }
-        else
-        {
-
-            enemymaxDir = new Vector3(-Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians), 0);
-        }
-
-        return enemymaxDir;
-    }
     private void TurnEnemytoPlayer()
     {
         //Flips enemy according to the Player position
@@ -251,16 +302,25 @@ public class EnemyControl : MonoBehaviour
         Debug.Log("FirstPhase");
 
         // calculating the distance between enemy position and player position
-
+        toPlayer = (Player.position - transform.position).normalized;
+        Vector2 Forward = FacingRight ? Vector2.right : Vector2.left;
         Vector2 EnemyPosition = new Vector2(transform.position.x, transform.position.y);
         Vector2 PlayerPosition = new Vector2(Player.gameObject.transform.position.x, Player.gameObject.transform.position.y);
         float distanceToPlayer = Vector2.Distance(EnemyPosition, PlayerPosition);
-        Debug.Log("Distance" + distanceToPlayer);
+        //float angleToPlayer = Vector2.SignedAngle(Forward, toPlayer);
+        //Debug.Log("angleToPlayer" + angleToPlayer);
 
-        // Checks the Distance between player and enemy is less than declared radius to figure out player is inside the boundary or not
+        //// Checks the Distance between player and enemy is less than declared radius to figure out player is inside the boundary or not
 
-        if (distanceToPlayer <= FovRadius)
+        //if (distanceToPlayer <= FovRadius && Mathf.Abs(angleToPlayer) <= FovAngle / 2f)
+        float angleToPlayer = Vector2.SignedAngle(Forward, toPlayer);
+        //Debug.Log("angleToPlayer" + angleToPlayer);
+        bool playerinFOV = distanceToPlayer <= FovRadius && Mathf.Abs(angleToPlayer) <= FovAngle / 2f;
+        bool ChaseRange = (distanceToPlayer <= PatrolDistance);
+        Debug.Log(distanceToPlayer);
+        if (playerinFOV && !isChase)
         {
+            isChase = true;
             IsPlayerEntered = true;
             CanPatrol = false;
             Debug.Log("Player Inside Boundary");
@@ -273,22 +333,36 @@ public class EnemyControl : MonoBehaviour
                 await Attack();
                 isAttacking = false;
             }
+                 
         }
-        else
+        else if(isChase && ChaseRange)
+            {
+                CanPatrol = false;
+                TurnEnemytoPlayer();
+                if (!isAttacking && attackTimer <= 0f)
+                {
+                    isAttacking = true;
+                    await Attack();
+                    isAttacking = false;
+                }
+
+            }
+            else
         {
             CanPatrol = true;
             IsPlayerEntered = false;
-            
+
         }
-       
+
     }
-   private async Awaitable Attack()
+    private async Awaitable Attack()
     {
-        //Projectile initialed from ProjectileBehaviour after 1f; of attack animation set to true and animation diabled after 0.5f of projectile initialization
+        /*Projectile initiated from ProjectileBehaviour after 1f sec of attack animation set to true
+        /and animation diabled after 0.5f of projectile initialization*/
         EnemyAimation.SetBool("IsSplitterAttack", true);
         await Awaitable.WaitForSecondsAsync(1f);
         ProjectileBehaviour projectile = Instantiate(Projectile, ProjectilePos.position, ProjectilePos.rotation).GetComponent<ProjectileBehaviour>();
-        projectile.InitializeProjectile(Player, Projectileforce); 
+        projectile.InitializeProjectile(Player, Projectileforce);
         await Awaitable.WaitForSecondsAsync(0.5f);
         EnemyAimation.SetBool("IsSplitterAttack", false);
         EnemyAimation.SetBool("isSplitterAttackDown", true);
